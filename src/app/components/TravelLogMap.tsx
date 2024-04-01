5e8ff5db8705f95a0ed853f594cbd252ba9999ba
Yasir Hassan <yasirkhilji64@gmail.com>
2024-04-01 17:19:53 +0500

    chore: Marker Popup

--- src/app/components/TravelLogMap.tsx
+++ src/app/components/TravelLogMap.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useMemo, useRef } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Map, { MapRef, Marker, NavigationControl, Popup } from 'react-map-gl';
 
@@ -12,39 +12,41 @@ interface TravelLogMapProps {
 }
 
 function TravelLogMap({ logs }: TravelLogMapProps) {
+  const [popupInfo, setPopupInfo] = useState<TravelLogWithId | null>(null);
   const mapRef = useRef<MapRef | null>(null);
   const pins = useMemo(
     () =>
-      logs.map((log, index) => (
-        <>
-          <Marker
-            key={index}
-            longitude={log.longitude}
-            latitude={log.latitude}
-            anchor="top"
-            onClick={(e) => {
-              mapRef.current?.flyTo({
-                animate: true,
-                center: e.target.getLngLat(),
-                zoom: 10.5,
-                speed: 1,
-              });
-            }}
-          >
-            <Pin />
-          </Marker>
-        </>
+      logs.map((log) => (
+        <Marker
+          key={log._id.toString()}
+          longitude={log.longitude}
+          latitude={log.latitude}
+          anchor="top"
+          onClick={(e) => {
+            mapRef.current?.flyTo({
+              animate: true,
+              center: e.target.getLngLat(),
+              zoom: 10.5,
+              speed: 1,
+            });
+            e.originalEvent.stopPropagation();
+            setPopupInfo(log);
+          }}
+        >
+          <Pin />
+        </Marker>
       )),
     [logs]
   );
   return (
     <Map
       initialViewState={{
-        latitude: 48.893,
-        longitude: -90.543,
-        zoom: 3.5,
+        latitude: 0,
+        longitude: 0,
+        zoom: 2.5,
       }}
-      projection={{ name: 'mercator' }}
+      minZoom={2.5}
+      projection={{ name: 'globe' }}
       mapStyle="mapbox://styles/mapbox/outdoors-v12"
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
       style={{ width: '100vw', height: '100vh' }}
@@ -52,28 +54,23 @@ function TravelLogMap({ logs }: TravelLogMapProps) {
       ref={mapRef}
     >
       {pins}
-
-      <Popup
-        anchor="top"
-        longitude={-90.545}
-        latitude={48.893}
-        closeButton={false}
-        className="size-auto"
-        maxWidth="10%"
-      >
-        <div>
-          <a
-            target="_new"
-            href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=pakistan`}
-          >
-            Wikipedia
-          </a>
-          <img
-            width="100%"
-            src="https://avatars.githubusercontent.com/u/36379648?s=400&u=ce710b45428f50fac66c6e439da7b5ad287d1f06&v=4"
-          />
-        </div>
-      </Popup>
+      {popupInfo && (
+        <Popup
+          anchor="bottom"
+          longitude={Number(popupInfo.longitude)}
+          latitude={Number(popupInfo.latitude)}
+          closeButton={false}
+          className="size-auto"
+          maxWidth="15%"
+          onClose={() => setPopupInfo(null)}
+        >
+          <div>
+            <h1 className="text-lg">{popupInfo.title}</h1>
+            <p className="line-clamp-2 text-sm">{popupInfo.description}</p>
+            <img width="100%" src={popupInfo.image} />
+          </div>
+        </Popup>
+      )}
       <NavigationControl
         position="top-right"
         showCompass={true}
